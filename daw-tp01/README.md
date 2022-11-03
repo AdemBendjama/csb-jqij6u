7dae6a1c60bb3d40858e104c85e1d658422803d4
Code Sandbox <hello@codesandbox.io>
2022-11-03 20:39:48 +0000

    Deployment from Codesandbox.

---
+++ daw-tp01/README.md
@@ -0,0 +1,24 @@
+# DAW-TP01
+
+Web Dev College Project
+
+# Github Repo
+
+https://github.com/AdemBendjama/csb-jqij6u
+
+# Site Link
+
+https://adembendjama.github.io/csb-jqij6u/
+
+# Profiles you can login with
+
+1. wm.facntic@univ-constantine2.dz        => password: webmaster => [ Admin   Account ]
+2. meriem.belguidoum@univ-constantine2.dz => password: meriembel => [ Teacher Account ]
+3. zen.bendjamaaadam2013@gmail.com        => password: adamben   => [ Student Account ]
+
+## note
+clear the localStorage for the browser and refresh if you want to reset the account and database to the original database
+
+# Education Website Template
+
+## LoginPage 
--- daw-tp01/index.js
+++ daw-tp01/index.js
@@ -1,32 +1,47 @@
-let users = [
-  {
-    name: "Web Master",
-    email: "wm.facntic@univ-constantine2.dz",
-    password: "webmaster",
-    type: "Admin"
-  },
-  {
-    name: "Adem Bendjama",
-    email: "adambendjamaa2013@gmail.com",
-    password: "adamben",
-    type: "Student"
-  },
-  {
-    name: "Meriem Belguidoum",
-    email: "meriem.belguidoum@univ-constantine2.dz",
-    password: "meriembel",
-    type: "Teacher"
-  }
-];
 // Assign a privilege character to each email
 // A => Admin
 // T => Teacher
 // S => Student
 
+let users = [
+];
+
+storageToList();
+
+/* add the adming account to the list */
+
+users.push({
+  name: "Web Master",
+  email: "wm.facntic@univ-constantine2.dz",
+  password: "webmaster",
+  type: "Admin"
+});
+
+/*================================================
+ local Storage Options */
+
+/* grab the local storage and transform it into a js array */
+function storageToList() {
+    //
+    let index = localStorage.getItem("index") - 1;
+
+    for (let i = 0; i <= index; i++) {
+        let user = {
+            name: localStorage.getItem("name" + i),
+            email: localStorage.getItem("email" + i),
+            password: localStorage.getItem("password" + i),
+            type: localStorage.getItem("type" + i)
+        }
+        users.push(user);
+    }
+}
+
+
 /* LOGIN */
 
 // Verify email and password input and redirect
 function login() {
+ 
   // get email and password value from input field in html
   let emailFetched = document.getElementById("email").value;
   let passwordFetched = document.getElementById("password").value;
@@ -37,6 +52,7 @@ function login() {
 
   // Compare the email and password entered with the table
   for (const user of users) {
+    
     if (user.email === emailFetched && user.password === passwordFetched) {
       alert("Login Successfull !");
 
@@ -44,28 +60,31 @@ function login() {
       window.location = redirect(emailFetched);
       infoCheck = true;
       break;
+
     }
   }
 
   // clear email and password field and display msg
   if (!infoCheck) {
+    
     alert("Login Unsuccessfull !");
     document.getElementById("email").value = "";
     document.getElementById("password").value = "";
+
   }
 }
 
 // redirect to the page based on the privilege lvl of the email
 function redirect(emailFetched) {
+ 
   // get the type of access the email has from the table
-  let typeOfPrivilege = users
-    .find((element) => element.email === emailFetched)
-    .type.toString();
+  let typeOfPrivilege = users.find(element => element.email === emailFetched).type.toString();
 
   // compare it to all privilege types Admin,Teacher,Student
   // return the link to the page of equal access
   // admin goes to 'accueilAdmin.html' page
   switch (typeOfPrivilege) {
+
     case "Admin":
       return "SubPages/Admin/AdminMain.html";
 
@@ -74,17 +93,25 @@ function redirect(emailFetched) {
 
     case "Student":
       return "SubPages/Student/StudentMain.html";
+      
   }
+
 }
 
 /* LOGIN ON ENTER KEY */
 
 document.querySelectorAll(".input-field").forEach((item) => {
+ 
   item.addEventListener("keyup", function (event) {
+    
     event.preventDefault();
-
+    
     if (event.key === "Enter") {
       document.getElementById("button").click();
     }
+
   });
+
 });
+
+
